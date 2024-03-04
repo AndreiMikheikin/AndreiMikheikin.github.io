3476a1a20904ca6ddd737e26c9284d43fa69588e
AndreiMikheikin <149661314+AndreiMikheikin@users.noreply.github.com>
2024-03-05 01:55:57 +0300

    js butyfy

--- js/site.js
+++ js/site.js
@@ -1,9 +1,55 @@
-window.addEventListener("load",function(){var e=document.getElementById("preloader");e.style.display="none"}),$(document).ready(function(e){e("#top>header>#mobMenu>.close-btn").click(function(){e("#top>header>.mobile-menu").css("display","none")})}),$(document).ready(function(e){e("#top>header>.container>.mobile-column>.mobile-column-btn").click(function(){e("#top>header>.mobile-menu").css("display","block")})}),$(document).ready(function(){var e=document.querySelector(".not-droped1"),o=document.querySelector(".dropdown-btn1");o.onclick=function(){e.classList.toggle("droped")}}),$(document).ready(function(){var e=document.querySelector(".not-droped2"),o=document.querySelector(".dropdown-btn2");o.onclick=function(){e.classList.toggle("droped")}}),$(document).ready(function(){var e=document.querySelector(".not-droped3"),o=document.querySelector(".dropdown-btn3");o.onclick=function(){e.classList.toggle("droped")}}),$(document).ready(function(){var e=document.querySelector(".not-droped4"),o=document.querySelector(".dropdown-btn4");o.onclick=function(){e.classList.toggle("droped")}});
+window.addEventListener("load",function(){
+    var e=document.getElementById("preloader");
+    e.style.display="none"
+});
+
+$(document).ready(function(e){
+    e("#top>header>#mobMenu>.close-btn").click(function(){
+        e("#top>header>.mobile-menu").css("display","none")
+    });
+});
+
+$(document).ready(function(e){
+    e("#top>header>.container>.mobile-column>.mobile-column-btn").click(function(){
+        e("#top>header>.mobile-menu").css("display","block")
+    });
+});
 
 $(document).ready(function(){
-    $('.accordion-btn').click(function(e){
-        var e = $(this)
-        e.next('.accordion-content').slideToggle(500);
+    var e=document.querySelector(".not-droped1");
+    o=document.querySelector(".dropdown-btn1");
+    o.onclick=function(){
+        e.classList.toggle("droped")
+    };
+});
+
+$(document).ready(function(){
+    var e=document.querySelector(".not-droped2");
+    o=document.querySelector(".dropdown-btn2");
+    o.onclick=function(){
+        e.classList.toggle("droped")
+    };
+});
+
+$(document).ready(function(){
+    var e=document.querySelector(".not-droped3");
+    o=document.querySelector(".dropdown-btn3");
+    o.onclick=function(){
+        e.classList.toggle("droped")
+    };
+});
+
+$(document).ready(function(){
+    var e=document.querySelector(".not-droped4");
+    o=document.querySelector(".dropdown-btn4");
+    o.onclick=function(){
+        e.classList.toggle("droped")
+    };
+});
+
+$(document).ready(function(){
+    $('.accordion-btn').click(function(){
+        $(this).next().slideToggle(500);
     });
 });
 
